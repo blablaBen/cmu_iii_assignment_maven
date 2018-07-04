a03b67dc00cd5621ddfad4a18e12f9d9ddde4d73
PATTARASAI <PATTARASAI@set.or.th>
2018-07-04 22:42:31 +0700

    add parseRatingInput()

--- src/main/java/com/cmu/task/App.java
+++ src/main/java/com/cmu/task/App.java
@@ -1,12 +1,16 @@
 package com.cmu.task;
 
+import com.cmu.task.domain.RatingInputItem;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Hello world!
@@ -24,5 +28,33 @@ public class App
                 .withHeader("UserID", "UserName", "UserAge", "MovieID", "MovieName", "Rating")
                 .withIgnoreHeaderCase()
                 .withTrim());
+
+        List<RatingInputItem> ratingInputWithMovieNameAndId = parseRatingInput(csvParser);
+        
+    }
+
+    public static List<RatingInputItem> parseRatingInput(CSVParser csvParser) {
+        List result = new ArrayList<RatingInputItem>();
+
+        for (CSVRecord csvRecord : csvParser) {
+            RatingInputItem item = new RatingInputItem();
+            item.userID = csvRecord.get("UserID");
+            item.userName = csvRecord.get("UserName");
+            item.userAge = csvRecord.get("UserAge");
+            item.movieID = extractMovieId(csvRecord.get("MovieName"));
+            item.movieName = extractMovieName(csvRecord.get("MovieName"));
+            item.rating = csvRecord.get("Rating");
+            result.add(item);
+        }
+
+        return result;
+    }
+
+    public static String extractMovieId(String movieIdAndName) {
+        return movieIdAndName.substring(0, movieIdAndName.indexOf(','));
+    }
+
+    public static String extractMovieName(String movieIdAndName) {
+        return movieIdAndName.substring(movieIdAndName.indexOf(',')+1, movieIdAndName.length());
     }
 }
---
+++ src/main/java/com/cmu/task/domain/RatingInputItem.java
@@ -0,0 +1,15 @@
+package com.cmu.task.domain;
+
+public class RatingInputItem {
+    public String userID;
+    public String userName;
+    public String userAge;
+    public String movieID;
+    public String movieName;
+    public String rating;
+
+
+    public RatingInputItem() {
+
+    }
+}
